e23dd004d6e8f674166f9c367710e83f47a95760
Blade <bladeprojectz@gmail.com>
2024-04-10 09:44:26 +0800

    fixed table bugs in CurrentPosition.jsx and TransactionList.jsx by adding <thead> and <tbody>'s

--- src/CurrentPositions.jsx
+++ src/CurrentPositions.jsx
@@ -3,33 +3,37 @@ function CurrentPositions() {
     <div>
       <h2>Current Positions</h2>
       <table>
-        <tr>
-          <th>Ticker</th>
-          <th>Amount</th>
-          <th>Purchase Price</th>
-          <th>Total Invested</th>
-          <th>Current Price</th>
-          <th>Profit/Loss</th>
-          <th>Percentage</th>
-        </tr>
-        <tr>
-          <td>SPY</td>
-          <td>5</td>
-          <td>$716</td>
-          <td>$3580</td>
-          <td>$801</td>
-          <td>$425</td>
-          <td>11.8%</td>
-        </tr>
-        <tr>
-          <td>AAPL</td>
-          <td>1</td>
-          <td>$150</td>
-          <td>$150</td>
-          <td>$171</td>
-          <td>$21</td>
-          <td>13.1%</td>
-        </tr>
+        <thead>
+          <tr>
+            <th>Ticker</th>
+            <th>Amount</th>
+            <th>Purchase Price</th>
+            <th>Total Invested</th>
+            <th>Current Price</th>
+            <th>Profit/Loss</th>
+            <th>Percentage</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>SPY</td>
+            <td>5</td>
+            <td>$716</td>
+            <td>$3580</td>
+            <td>$801</td>
+            <td>$425</td>
+            <td>11.8%</td>
+          </tr>
+          <tr>
+            <td>AAPL</td>
+            <td>1</td>
+            <td>$150</td>
+            <td>$150</td>
+            <td>$171</td>
+            <td>$21</td>
+            <td>13.1%</td>
+          </tr>
+        </tbody>
       </table>
     </div>
   );
--- src/TransactionsList.jsx
+++ src/TransactionsList.jsx
@@ -3,18 +3,22 @@ function TransactionList() {
     <div>
       <h2>Transaction History</h2>
       <table>
-        <tr>
-          <th>Ticker</th>
-          <th>Amount</th>
-          <th>Purchase Price</th>
-          <th>Total Invested</th>
-        </tr>
-        <tr>
-          <td>SPY</td>
-          <td>5</td>
-          <td>$716</td>
-          <td>$3580</td>
-        </tr>
+        <thead>
+          <tr>
+            <th>Ticker</th>
+            <th>Amount</th>
+            <th>Purchase Price</th>
+            <th>Total Invested</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>SPY</td>
+            <td>5</td>
+            <td>$716</td>
+            <td>$3580</td>
+          </tr>
+        </tbody>
       </table>
     </div>
   );

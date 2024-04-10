037f58bf43227dbf70e393178b7eedcc278e4b00
Blade <bladeprojectz@gmail.com>
2024-04-10 10:03:32 +0800

    moved transactions fetch to TransactionsList.jsx

--- src/TransactionsList.jsx
+++ src/TransactionsList.jsx
@@ -1,10 +1,27 @@
+import { useState } from "react";
+
 function TransactionList() {
+  const [transactions, setTransactions] = useState(fetchTransactions);
+
+  function fetchTransactions() {
+    fetch("http://localhost:3000/transactions", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => setTransactions(data));
+  }
+
+  console.log(transactions);
   return (
     <div>
       <h2>Transaction History</h2>
       <table>
         <thead>
           <tr>
+            <th>Action</th>
             <th>Ticker</th>
             <th>Amount</th>
             <th>Purchase Price</th>
@@ -13,6 +30,7 @@ function TransactionList() {
         </thead>
         <tbody>
           <tr>
+            <td>BUY</td>
             <td>SPY</td>
             <td>5</td>
             <td>$716</td>
